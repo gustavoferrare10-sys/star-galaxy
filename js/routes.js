b41dd58ab3287d2a9d24533ba89719458f879f1b
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 21:57:08 -0300

    routes.js

---
+++ js/routes.js
@@ -0,0 +1,19 @@
+console.log("ROTAS CARREGADAS");
+
+const routes = new PIXI.Graphics();
+galaxy.addChild(routes);
+
+function drawRoute(p1, p2) {
+  routes.lineStyle(2, 0x00ffff, 0.6);
+  routes.moveTo(p1.x, p1.y);
+  routes.lineTo(p2.x, p2.y);
+}
+
+// exemplos
+drawRoute(planetsData[0], planetsData[1]);
+drawRoute(planetsData[1], planetsData[2]);
+
+// animação simples
+app.ticker.add(() => {
+  routes.alpha = 0.5 + Math.sin(Date.now() * 0.003) * 0.2;
+});
