062b2bc7467292cc29c4262f7dc46788c1b190d4
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 22:29:24 -0300

    Delete js/panel.js

--- js/panel.js
+++
@@ -1,9 +0,0 @@
-function openPlanetPanel(p) {
-  document.getElementById("planet-name").innerText = p.name;
-  document.getElementById("planet-desc").innerText = p.description;
-  document.getElementById("planet-panel").classList.remove("hidden");
-}
-
-function closePanel() {
-  document.getElementById("planet-panel").classList.add("hidden");
-}
