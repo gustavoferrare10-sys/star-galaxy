d40da4342d93b185cff20d0804143e8376ca5988
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 21:30:16 -0300

    data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,25 @@
 console.log("DATA.JS CARREGADO");
 
 window.planetsData = [
-  { name: "Tatooine", x: -300, y: 150, region: "Outer Rim", description: "Planeta desértico" },
-  { name: "Coruscant", x: 200, y: -100, region: "Core Worlds", description: "Capital galáctica" },
-  { name: "Naboo", x: 0, y: -250, region: "Mid Rim", description: "Planeta aquático" }
+  {
+    name: "Tatooine",
+    x: -300,
+    y: 150,
+    region: "Outer Rim",
+    image: "assets/planets/tatooine.png"
+  },
+  {
+    name: "Coruscant",
+    x: 200,
+    y: -100,
+    region: "Core Worlds",
+    image: "assets/planets/coruscant.png"
+  },
+  {
+    name: "Naboo",
+    x: 0,
+    y: -250,
+    region: "Mid Rim",
+    image: "assets/planets/naboo.png"
+  }
 ];
