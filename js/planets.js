6a39505f02490dceb6dd21f101d39f4664d90f05
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 21:14:09 -0300

    planets.js

--- js/planets.js
+++ js/planets.js
@@ -20,3 +20,34 @@ window.planetsData.forEach((p, i) => {
   console.log("Planeta criado:", p.name);
 });
 
+planetsData.forEach(p => {
+  const planet = new PIXI.Graphics();
+
+  planet.beginFill(0x00ffff);
+  planet.drawCircle(0, 0, 25);
+  planet.endFill();
+
+  planet.x = p.x;
+  planet.y = p.y;
+
+  planet.interactive = true;
+  planet.buttonMode = true;
+
+  // Glow fake (scale + alpha)
+  planet.on("pointerover", () => {
+    planet.scale.set(1.2);
+    planet.alpha = 1;
+  });
+
+  planet.on("pointerout", () => {
+    planet.scale.set(1);
+    planet.alpha = 0.9;
+  });
+
+  planet.on("pointerdown", () => {
+    openPlanetPanel(p);
+  });
+
+  galaxy.addChild(planet);
+});
+
