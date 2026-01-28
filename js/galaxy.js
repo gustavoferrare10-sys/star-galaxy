e76e5a1e612ad8f8597b59a870b8b36c59bdb2a4
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 21:11:57 -0300

    galaxy.js

--- js/galaxy.js
+++ js/galaxy.js
@@ -11,3 +11,18 @@ for (let i = 0; i < 1000; i++) {
 }
 
 galaxy.addChild(stars);
+
+const grid = new PIXI.Graphics();
+grid.lineStyle(1, 0x333333);
+
+for (let x = -1000; x <= 1000; x += 100) {
+  grid.moveTo(x, -1000);
+  grid.lineTo(x, 1000);
+}
+
+for (let y = -1000; y <= 1000; y += 100) {
+  grid.moveTo(-1000, y);
+  grid.lineTo(1000, y);
+}
+
+galaxy.addChild(grid);
