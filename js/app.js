38350059754e1ec83832c7baf9980ed8854ef45a
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 22:27:44 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
-const app = new PIXI.Application({
-  resizeTo: window,
-  backgroundColor: 0x000000,
-  antialias: true
-});
 
-document.body.appendChild(app.view);
-
-// Container principal
-const galaxy = new PIXI.Container();
-app.stage.addChild(galaxy);
-
-// Centraliza o mapa
-galaxy.x = app.screen.width / 2;
-galaxy.y = app.screen.height / 2;
-galaxy.scale.set(1);
