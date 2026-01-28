cc9ef65d82a7ec6dd51a3f639a4fbc4ca303654d
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 21:04:40 -0300

    app.js
    Removed debug visual elements from the galaxy container.

--- js/app.js
+++ js/app.js
@@ -10,13 +10,6 @@ document.body.appendChild(app.view);
 const galaxy = new PIXI.Container();
 app.stage.addChild(galaxy);
 
-// DEBUG VISUAL (remove depois)
-const test = new PIXI.Graphics();
-test.beginFill(0xff0000);
-test.drawCircle(0, 0, 20);
-test.endFill();
-galaxy.addChild(test);
-
 // Centraliza o mapa
 galaxy.x = app.screen.width / 2;
 galaxy.y = app.screen.height / 2;
