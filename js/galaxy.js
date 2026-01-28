e2129626d96d368fee0d43bebd0e191a0ac77e49
gustavoferrare10-sys <gustavoferrare10@gmail.com>
2026-01-27 22:33:54 -0300

    galaxy.js

--- js/galaxy.js
+++ js/galaxy.js
@@ -1,12 +1,20 @@
 console.log("GALAXY.JS INICIANDO");
 
 window.app = new PIXI.Application({
-  resizeTo: window,
+  width: window.innerWidth,
+  height: window.innerHeight,
   backgroundColor: 0x000000,
   antialias: true
 });
 
 document.body.appendChild(app.view);
+
+// Responsivo ao redimensionar
+window.addEventListener('resize', () => {
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+  galaxy.x = window.innerWidth / 2;
+  galaxy.y = window.innerHeight / 2;
+});
 
 window.galaxy = new PIXI.Container();
 app.stage.addChild(galaxy);
